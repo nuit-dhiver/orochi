c15aca1b7722d181b523d94b976913f040ede337
Sai Mohammad-Hossein Emami <emami@outlook.com>
2023-03-23 13:35:34 +0330

    Create definitions.rs

---
+++ src/definitions.rs
@@ -0,0 +1,6 @@
+let struct Entity { key: Uuid, value: String }
+
+fn insert_entity(passed_value: &str) {
+    let new_record = { key: Uuid::new_v4(), value String::from(passed_value)}
+    // countinue
+}
