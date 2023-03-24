1d1bef6cca25ab058ba2b54c303e5a54d52f442c
Sai Mohammad-Hossein Emami <emami@outlook.com>
2023-03-24 15:01:47 +0330

    Added some lines to definitions

--- src/definitions.rs
+++ src/definitions.rs
@@ -1,6 +1,17 @@
-let struct Entity { key: Uuid, value: String }
+use std::fs::File;
+use std::io::prelude::*;
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+
+let struct Entity { 
+    key: Uuid, 
+    value: String 
+}
 
 fn insert_entity(passed_value: &str) {
     let new_record = { key: Uuid::new_v4(), value String::from(passed_value)}
-    // countinue
+    let encoded: Vec<u8> = bincode::serialize(&new_record).unwrap()
+    //continiue
 }
