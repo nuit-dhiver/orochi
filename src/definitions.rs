6d0f77d4948d658853bdf85aa739f789127a710d
Sai Mohammad-Hossein Emami <emami@outlook.com>
2023-03-26 15:23:40 +0330

    implemented insert_entity with json

--- src/definitions.rs
+++ src/definitions.rs
@@ -1,20 +1,14 @@
 use std::fs::File;
 use std::io::prelude::*;
+use serde_json::{json, value};
+use uuid::Uuid;
 
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-
-let struct Entity { 
-    key: Uuid, 
-    value: String 
-}
-
-fn insert_entity(passed_value: &str) -> Result<()> {
-    let new_record = { key: Uuid::new_v4(), value String::from(passed_value)}
-    //let encoded: Vec<u8> = bincode::serialize(&new_record).unwrap()
-    let mut entity_file = File::open(./data/entity)?;
-    let buffer = serde_bincode::serialize(&new_record)?;
-    entity_file.write_all(&buffer[..])?;
+fn insert_entity(value: Value) -> Result<(), Box<dyn std::error::Error>> {
+    let uuid = Uuid::new_v4();
+    let mut object = json!({"uuid": uuid.to_hyphenated().to_string()});
+    object["data"] = value;
+    let json_string = serde_json::to_string_pretty(&object)?;
+    let mut entity_file = OpenOptions::new().append(true).open("entities.json")?;
+    entity_file.write_all(json_string.as_bytes())?;
     Ok(())
 }
