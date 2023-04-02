66dee78bd204a1f7cb97f7cb7cef89ba503b263b
Sai Mohammad-Hossein Emami <emami@outlook.com>
2023-04-02 15:17:26 +0330

    interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod takeoff;
 mod operations;
+use std::io::{self, Write};
+use serde_json::{Value};
+use regex::Regex;
 
 
 fn main() {
@@ -8,5 +11,17 @@ fn main() {
         return;
     }
     
-    operations::insert_entity("{}".into());
+    loop {
+        let mut input = String::new();
+        print!(">: ");
+        io::stdout().flush().expect("Failed to flush stdout");
+        io::stdin().read_line(&mut input).expect("Failed to read line.");
+        input = input.trim().to_string();
+
+        let re = Regex::new(r#"^(insert|pick)\((.*)\)$"#).unwrap();
+        if let Some(captures) = re.captures(&input) {
+            let function = captures.get(1).unwrap().as_str();
+            let arg_string = captures.get(2).unwrap().as_str();
+        }
+    }
 }
